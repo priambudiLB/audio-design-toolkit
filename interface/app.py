b954077173e634146678c76a5732442d1164d9aa
Priambudi Lintang Bagaskara <priambudibagaskara@gmail.com>
2023-06-12 11:39:51 +0800

    react template

--- interface/app.py
+++ interface/app.py
@@ -1,6 +1,7 @@
 from multipage import MultiPage
 from perceptual_guided_control import main as pg_control_main
 from query_gan import main as query_gan_main
+from query_gan_react import main as query_gan_react
 from sefa import main as sefa_main
 
 
@@ -11,5 +12,6 @@ import streamlit as st
 app = MultiPage()
 app.add_app('our-algo', pg_control_main)
 app.add_app('query-gan', query_gan_main)
+app.add_app('query-gan-react', query_gan_react)
 app.add_app('sefa-algo', sefa_main)
 app.run()
