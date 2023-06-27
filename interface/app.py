3882312382dfe8ac5a4c2029f5b86ec928f5710d
Priambudi Lintang Bagaskara <priambudibagaskara@gmail.com>
2023-06-27 08:42:50 +0800

    sefa algo ui

--- interface/app.py
+++ interface/app.py
@@ -3,6 +3,7 @@ from perceptual_guided_control import main as pg_control_main
 from query_gan import main as query_gan_main
 from query_gan_react import main as query_gan_react
 from sefa import main as sefa_main
+from sefa_react import main as sefa_react
 #from GaverAnalysisSynthesis import main as GaverAnalysisSynthesisMain
 #from SefaInterface import main as SefaInterfaceMain
 
@@ -16,5 +17,6 @@ app.add_app('our-algo', pg_control_main)
 app.add_app('algo1', query_gan_main)
 app.add_app('algo2', sefa_main)
 app.add_app('algo1-react', query_gan_react)
+app.add_app('algo2-react', sefa_react)
 app.run()
 
