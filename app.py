709a57c8d64c57d6b4d1aa67795c16dddf2d3ce2
Priambudi Lintang Bagaskara <priambudibagaskara@gmail.com>
2023-07-05 11:56:49 +0800

    update req

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from multipage import MultiPage
-from perceptual_guided_control import main as pg_control_main
-from query_gan import main as query_gan_main
-from query_gan_react import main as query_gan_react
-from sefa import main as sefa_main
-from sefa_react import main as sefa_react
+from interface.multipage import MultiPage
+from interface.perceptual_guided_control import main as pg_control_main
+from interface.query_gan import main as query_gan_main
+from interface.query_gan_react import main as query_gan_react
+from interface.sefa import main as sefa_main
+from interface.sefa_react import main as sefa_react
 #from GaverAnalysisSynthesis import main as GaverAnalysisSynthesisMain
 #from SefaInterface import main as SefaInterfaceMain
 
